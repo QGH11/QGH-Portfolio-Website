e620065cdbb4540daaada974354a02e732b4fa3f
QGh11 <Guanhua.Qiao@outlook.com>
2022-12-30 22:14:32 -0800

    roughly done intro, starting about me

--- client/app.js
+++ client/app.js
@@ -1,6 +1,6 @@
 /*  */
-class IntroAnimation {
-    constructor(scene, camera, renderer, htmlControl) {
+class Animation {
+    constructor(scene, camera, renderer, htmlControl, swordCharacter) {
         this.scene = scene;
         this.camera = camera;   
         this.renderer = renderer;
@@ -8,10 +8,13 @@ class IntroAnimation {
 
         this.htmlControl = htmlControl;
 
+        this.swordCharacter = swordCharacter;
+
         this.coin1 = null;
         this.coin2 = null;
 
-        this.control = new THREE_ADDONS.OrbitControls(this.camera, this.renderer.domElement);
+        // state for controling html and three.js elements
+        this.state = "Intro"; // "Intro" <-> "AboutMe" <-> "Projects" <-> "Contact"
 
         // dynamically changed scene sizes when window resizes
         window.addEventListener( 'resize', onWindowResize, false );
@@ -104,60 +107,98 @@ class IntroAnimation {
 
         animate()
     }
-
+    
     /*  */
-    sceneControl(sword) {
-        // camera
+    sceneControl() {
         var self = this;
-        var t1 = gsap.timeline();
-        t1  .to(this.camera.position, {duration: 3, z: 5}, "flipCoin")
-            .to(sword.position, {duration: 3, z: -2}, "flipCoin");
-        t1  .to(this.coin1.position, {duration: 1, x: -30}, "sliceCoin")
-            .to(this.coin2.position, {duration: 1, x: 30}, "sliceCoin")
-            .to(sword.position, {duration: 0.5, x: -20, y: -20}, "sliceCoin");
-        t1  .call(function() {
-                self.scene.remove(self.coin1);
-                self.scene.remove(self.coin2);
-            }, null)
-            .to(sword.position, {duration: 0.5, x: 0, y: 0})
-            .to(sword.rotation, {duration: 0.5, x: 0, y: 0, z:0}, "positionSword")
-            .to(sword.position, {duration: 0.5, x: -window.innerWidth / window.innerHeight * 7, y: 0}, "positionSword");
-        t1. call(function() {
-            self.htmlControl.displayByClassName("AboutMe-Card");
-            var i = 0;
-            var txt = 'Hello, I\'m QGH! Somehow I\'m trapped inside this sword...'; /* The text */
-            var speed = 50; /* The speed/duration of the effect in milliseconds */
-            self.htmlControl.typeWriter(txt, speed, "AboutMe-CardText");
-        });
+        var IntroTimmeline = gsap.timeline();
+        // var AboutMeTimelIne = gsap.timeline();
 
-        // dynamically changed object position based window/canvas size
-        window.addEventListener( 'resize', onWindowResize, false );
-        function onWindowResize() {
-            t1.to(sword.position, {duration: 0.5, x: -window.innerWidth / window.innerHeight * 7, y: 0});
-        };
+        this.swordCharacter.swordAction.then((sword) => {
+            if (!this.state.localeCompare("Intro")) {
+                // 1. flip Coin
+                IntroTimmeline  .to(this.camera.position, {duration: 3, z: 5}, "Init")
+                                .to(sword.position, {duration: 3, z: -2}, "Init")
+
+                // 2. Slice coini
+                IntroTimmeline  .to(this.coin1.position, {duration: 1, x: -30}, "sliceCoin")
+                                .to(this.coin2.position, {duration: 1, x: 30}, "sliceCoin")
+                                .to(sword.position, {duration: 0.5, x: -20, y: -20}, "sliceCoin");
+                
+                // 3. Recover sword position
+                IntroTimmeline  .call(function() {
+                                    self.scene.remove(self.coin1);
+                                    self.scene.remove(self.coin2);
+                                }, null)
+                                .to(sword.position, {duration: 0.5, x: 0, y: 0})
+                                .to(sword.rotation, {duration: 0.5, x: 0, y: 0, z:0}, "positionSword")
+                                .to(sword.position, {duration: 0.5, x: -window.innerWidth / window.innerHeight * 7, y: 0}, "positionSword");
+
+                // 4. Interact with HTML elements
+                IntroTimmeline  .call(async function() {
+                                    self.htmlControl.displayByClassName("Intro-Card");
+                                    var txt = 'Hello, I\'m QGH! Somehow I\'m trapped inside this sword...'; /* The text */
+                                    var speed = 50; /* The speed/duration of the effect in milliseconds */
+                                    await self.htmlControl.typeWriter(txt, speed, "Intro-CardText");
+
+                                    await sleep(1000);
+
+                                    self.htmlControl.displayByClassName("Intro-Card");
+                                    var txt = 'Scroll down to explore more!'; /* The text */
+                                    var speed = 50; /* The speed/duration of the effect in milliseconds */
+                                    await self.htmlControl.typeWriter(txt, speed, "Intro-CardText");
+                                }, null, ">");
+            }
+            else if (!this.state.localeCompare("AboutMe")) {
+                // allow sword to track mouse cursor
+                document.body.addEventListener('mousemove', (event) => {
+                    this.swordCharacter.onMouseMove(event, sword);
+                })
+            }
+
+
+            // dynamically changed object position based window/canvas size
+            window.addEventListener( 'resize', onWindowResize, false);
+            function onWindowResize() {
+                IntroTimmeline.to(sword.position, {duration: 0.5, x: -window.innerWidth / window.innerHeight * 7, y: 0});
+            };
+        })
     }   
 }
 
 class HTMLControl {
     constructor() {
-
+        this.canvas = document.getElementsByClassName("htmlContent-Container")[0];
     }
 
     /*  */
     typeWriter(txt, speed, id) {
+        //  empty text
+        document.getElementById(id).innerHTML = "";
+
         var i = 0;
         var txt = txt; /* The text */
         var speed = speed; /* The speed/duration of the effect in milliseconds */
 
-        function typeWriterHelper() {
-            if (i < txt.length) {
-                document.getElementById(id).innerHTML += txt.charAt(i);
-                i++;
-                setTimeout(typeWriterHelper, speed);
-            }
-        }
+        document.body.addEventListener("click", ()=> {
+            speed = 10;
+        })
 
-        typeWriterHelper();
+        return new Promise((resolve, reject) => {
+            function typeWriterHelper() {
+                if (i < txt.length) {
+                    document.getElementById(id).innerHTML += txt.charAt(i);
+                    i++;
+                    setTimeout(typeWriterHelper, speed);
+                }
+                else {
+                    resolve(true);
+                }
+            }
+
+            typeWriterHelper();
+        });
+
     }
 
     /*  */
@@ -165,7 +206,6 @@ class HTMLControl {
         var tagetNode = document.getElementsByClassName(className)[0];
         tagetNode.style.visibility = "visible";
         tagetNode.style.opacity = 1;
-
     }
 
     /*  */
@@ -184,29 +224,29 @@ class HTMLControl {
         return template.content.firstChild;
     }
 }
-window.addEventListener('load', main);
 
-/*  */
-function main() {  
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(125, window.innerWidth/window.innerHeight, 0.1, 1000);    
-    var renderer = new THREE.WebGLRenderer({antialias: true, canvas: document.querySelector("#bg")});
+class SwordCharacter {
+    constructor(scene, camera, renderer) {
+        this.swordAction = this.loadCharacter("./assets/3DObjects/rose_quartzs_sword/scene.gltf");
+        this.scene = scene;
+        this.camera = camera;
+        this.renderer = renderer;
 
-    var htmlControl = new HTMLControl();
-    var QGHIntro = new IntroAnimation(scene, camera, renderer, htmlControl);
+        this.plane = new THREE.Plane(new THREE.Vector3(0, 0, 1), 0);
+        this.raycaster = new THREE.Raycaster();
+        this.mouse = new THREE.Vector2();
+        this.pointOfIntersection = new THREE.Vector3(20,20,20);
+    }
 
-    QGHIntro.coinFlip();
-    loadCharacter("./assets/3DObjects/rose_quartzs_sword/scene.gltf");
-
-    function modelLoader(path) {
+    modelLoader(path) {
         const loader = new THREE.GLTFLoader();
         return new Promise((resolve, reject) => {
             loader.load(path, data=> resolve(data), null, reject);
         });
     }
 
-    async function loadCharacter(path) {
-        const gltf = await modelLoader(path);
+    async loadCharacter(path) {
+        const gltf = await this.modelLoader(path);
      
         var sword = gltf.scene.children[0]; 
         sword.scale.set(5,5,5);
@@ -219,8 +259,57 @@ function main() {
         sword.rotation.y = - Math.PI / 3;
         sword.rotation.z = - Math.PI / 2;
 
-        scene.add(sword);
+        this.scene.add(sword);
 
-        QGHIntro.sceneControl(sword);
+        return sword;
     }
+
+    onMouseMove(event, sword) {
+        this.mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+        this.mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+        this.raycaster.setFromCamera(this.mouse, this.camera);
+        this.raycaster.ray.intersectPlane(this.plane, this.pointOfIntersection);
+        // sword.lookAt(this.pointOfIntersection);
+        sword.position.set(this.pointOfIntersection.x, this.pointOfIntersection.y-5, sword.position.z);
+        sword.rotation.set(sword.rotation.x, sword.rotation.y, Math.tan(this.pointOfIntersection.x/this.pointOfIntersection.y * Math.PI / 180));
+
+    }
+}
+
+window.addEventListener('load', main);
+
+/*  */
+function main() {  
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(125, window.innerWidth/window.innerHeight, 0.1, 1000);    
+    var renderer = new THREE.WebGLRenderer({antialias: true, canvas: document.querySelector("#bg")});
+
+    var htmlControl = new HTMLControl();
+    var swordCharacter = new SwordCharacter(scene, camera, renderer);
+    var QGHAnimation = new Animation(scene, camera, renderer, htmlControl, swordCharacter);
+
+    QGHAnimation.coinFlip();
+    QGHAnimation.sceneControl();
+
+    document.getElementsByClassName("htmlContent-Container")[0].addEventListener("scroll", function() {
+        const top = (window.pageYOffset || this.scrollTop)  - (this.clientTop || 0);
+        // console.log(top);
+        // console.log(QGHAnimation.state);
+
+        // "Other State" -> "AboutMe"
+        if (QGHAnimation.state.localeCompare("AboutMe") && top >= 200) {
+            QGHAnimation.state = "AboutMe";
+            QGHAnimation.sceneControl();
+        }
+        // // "Other State" -> "Intro"
+        // else if (QGHAnimation.state.localeCompare("Intro") && top < 200) {
+        //     QGHAnimation.state = "Intro";
+        //     QGHAnimation.sceneControl();
+        // }
+    });
+}
+
+/*  */
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
