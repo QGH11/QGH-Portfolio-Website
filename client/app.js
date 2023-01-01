eb2e587c71a9cf4528af725661261a6ee34ae645
QGh11 <Guanhua.Qiao@outlook.com>
2022-12-31 23:07:31 -0800

    startinng abouut me, add bg

--- client/app.js
+++ client/app.js
@@ -4,7 +4,6 @@ class Animation {
         this.scene = scene;
         this.camera = camera;   
         this.renderer = renderer;
-        this.initializeCoinScene();
 
         this.htmlControl = htmlControl;
 
@@ -12,9 +11,16 @@ class Animation {
 
         this.coin1 = null;
         this.coin2 = null;
+        this.saturn = null;
+
+        this.particles = new THREE.Group();
+
+        // this.control = new THREE_ADDONS.OrbitControls(this.camera, this.renderer.domElement);
 
         // state for controling html and three.js elements
         this.state = "Intro"; // "Intro" <-> "AboutMe" <-> "Projects" <-> "Contact"
+
+        this.init();
 
         // dynamically changed scene sizes when window resizes
         window.addEventListener( 'resize', onWindowResize, false );
@@ -25,16 +31,33 @@ class Animation {
 
             self.renderer.setSize( window.innerWidth, window.innerHeight );
         }
+        
     }
 
     /*  */
-    initializeCoinScene() {
+    init() {
         this.renderer.setPixelRatio(window.devicePixelRatio);
         this.renderer.setSize(window.innerWidth, window.innerHeight);
         this.renderer.shadowMap.enabled = true;
         this.renderer.outputEncoding = THREE.sRGBEncoding;
         this.renderer.toneMappingExposure = 1;
-        this.scene.background = new THREE.Color( 0xdddddd);
+        this.scene.background = new THREE.Color( 0x343435);
+
+        const ambientLight = new THREE.AmbientLight();
+        this.scene.add(ambientLight);
+      
+        const light = new THREE.DirectionalLight();
+        light.position.set(200, 100, 200);
+        light.castShadow = true;
+        light.shadow.camera.left = -100;
+        light.shadow.camera.right = 100;
+        light.shadow.camera.top = 100;
+        light.shadow.camera.bottom = -100;
+        this.scene.add(light);
+
+        this.coinFlip();
+        this.drawSaturn();
+        this.drawParticles();
     }
 
     /* 
@@ -95,6 +118,8 @@ class Animation {
             if (self.camera.position.z === 5) {
                 self.coin1.rotation.x = 90;
                 self.coin2.rotation.x = 90;
+
+                // should remove light as well
             }
             else {
                 self.coin1.rotation.x +=0.015;
@@ -106,6 +131,93 @@ class Animation {
         }
 
         animate()
+    }
+
+
+    // better planet https://codepen.io/cluzier/pen/mGeEQV
+    /*  
+        https://codepen.io/elliezen/details/yMqqWe
+    */
+    drawSaturn() {
+        this.saturn = new THREE.Group();
+        this.saturn.rotation.set(0.4, 0.3, 0);
+        this.saturn.position.x = -1000;
+        this.saturn.position.z = -1000;
+        this.scene.add(this.saturn);
+        
+        const planetGeometry = new THREE.IcosahedronGeometry(50, 1);
+        
+        const planetMaterial = new THREE.MeshPhongMaterial({
+          color: 0x37BE95
+          });
+        const planet = new THREE.Mesh(planetGeometry, planetMaterial);
+        
+        planet.castShadow = true;
+        planet.receiveShadow = true;
+        planet.position.set(0, 40, 0);
+        this.saturn.add(planet);
+        
+        const ringGeometry = new THREE.TorusGeometry(100, 12, 6, 15);
+        const ringMeterial = new THREE.MeshStandardMaterial({
+          color: 0x6549C0
+        });
+        const ring = new THREE.Mesh(ringGeometry, ringMeterial);
+        ring.position.set(0, 40, 0)
+        ring.rotateX(80);
+        ring.castShadow = true;
+        ring.receiveShadow = true;
+        this.saturn.add(ring);
+    }
+    
+    /* https://codepen.io/elliezen/pen/yMqqWe */
+    drawParticles() {
+        this.scene.add(this.particles);
+        const geometry = new THREE.TetrahedronGeometry(5, 0);
+
+        const colors = [0x37BE95, 0xF3F3F3, 0x6549C0];
+        const starsNum = 500;
+
+        for (let i = 0; i < starsNum; i ++) {
+          const material = new THREE.MeshPhongMaterial({
+            color: colors[Math.floor(Math.random() * colors.length)]
+          });
+          const mesh = new THREE.Mesh(geometry, material);
+          mesh.position.set((Math.random() - 0.5) * 1000,
+                            (Math.random() - 0.5) * 1000,
+                            (Math.random() - 0.5) * 1000);
+          mesh.updateMatrix();
+          mesh.matrixAutoUpdate = false;
+          this.particles.add(mesh);
+        }
+
+        var self = this;
+        // slow startAnimation
+        function starAnimation() {
+            //   Stars Animation  
+            for (let i = 0; i < starsNum; i +=2) {
+                self.particles.children[i].translateX(0);
+                self.particles.children[i + 1].position.x += 0.005;
+                // if (self.particles.children[i].position.x > 10)
+                // l_velocity_Array[2 * i] += 0.0049;
+                // l_velocity_Array[2 * i + 1] += 0.005;
+
+                // l_vertex_Array[6 * i + 2] += l_velocity_Array[2 * i];
+                // l_vertex_Array[6 * i + 5] += l_velocity_Array[2 * i + 1];
+
+                // if (self.particles > 10) {
+                //     l_vertex_Array[6 * i + 2] = l_vertex_Array[6 * i + 5] = THREE.MathUtils.randInt(-200, 10);
+                //     l_velocity_Array[2 * i] = 0;
+                //     l_velocity_Array[2 * i + 1] = 0;
+                // }
+            }
+
+
+
+            requestAnimationFrame(starAnimation);
+            self.renderer.render(self.scene, self.camera);
+        }
+
+        starAnimation();
     }
     
     /*  */
@@ -150,10 +262,24 @@ class Animation {
                                 }, null, ">");
             }
             else if (!this.state.localeCompare("AboutMe")) {
+
+                // 1. Clean the Intro Card
+                IntroTimmeline  .call(this.htmlControl.hidebyClassName("Intro"));
+
+                // 2. Plan to start flying toward saturn
+                IntroTimmeline  .to(sword.position, {duration: 0.5, x: 5}, "pointSaturn")
+                                .to(sword.rotation, {duration: 0.5, x: -Math.PI / 2, y: Math.PI / 2}, "pointSaturn");
+
+                // 3. Ajust Camera, and start flying (bg start effects) and Introduce About me
+                IntroTimmeline  .to(this.camera.rotation, {duration: 0.5, y: -Math.PI / 2}, "adjustCamera")
+                                .to(this.camera.position, {duration: 0.5, z: 0}, "adjustCamera")
+                                // .call(this.startBackground())
+
+
                 // allow sword to track mouse cursor
-                document.body.addEventListener('mousemove', (event) => {
-                    this.swordCharacter.onMouseMove(event, sword);
-                })
+                // document.body.addEventListener('mousemove', (event) => {
+                //     this.swordCharacter.onMouseMove(event, sword);
+                // })
             }
 
 
@@ -208,6 +334,12 @@ class HTMLControl {
         tagetNode.style.opacity = 1;
     }
 
+    hidebyClassName(className) {
+        var tagetNode = document.getElementsByClassName(className)[0];
+        tagetNode.style.visibility = "hide";
+        tagetNode.style.opacity = 0;
+    }
+
     /*  */
     emptyDOM (elem){
         while (elem.firstChild) elem.removeChild(elem.firstChild);
@@ -235,7 +367,7 @@ class SwordCharacter {
         this.plane = new THREE.Plane(new THREE.Vector3(0, 0, 1), 0);
         this.raycaster = new THREE.Raycaster();
         this.mouse = new THREE.Vector2();
-        this.pointOfIntersection = new THREE.Vector3(20,20,20);
+        this.pointOfIntersection = new THREE.Vector3(0,0,0);
     }
 
     modelLoader(path) {
@@ -264,16 +396,21 @@ class SwordCharacter {
         return sword;
     }
 
-    onMouseMove(event, sword) {
-        this.mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-        this.mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-        this.raycaster.setFromCamera(this.mouse, this.camera);
-        this.raycaster.ray.intersectPlane(this.plane, this.pointOfIntersection);
-        // sword.lookAt(this.pointOfIntersection);
-        sword.position.set(this.pointOfIntersection.x, this.pointOfIntersection.y-5, sword.position.z);
-        sword.rotation.set(sword.rotation.x, sword.rotation.y, Math.tan(this.pointOfIntersection.x/this.pointOfIntersection.y * Math.PI / 180));
+    // onMouseMove(event, sword) {
+    //     // this.mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    //     // this.mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    //     // this.raycaster.setFromCamera(this.mouse, this.camera);
+    //     // this.raycaster.ray.intersectPlane(this.plane, this.pointOfIntersection);
+    //     // sword.position.set(this.pointOfIntersection.x, this.pointOfIntersection.y-5, sword.position.z);
 
-    }
+    //     // // this.sword.
+    //     // sword.rotation.set(sword.rotation.x, sword.rotation.y, Math.atan2(sword.position.y-this.mouse.y, sword.position.x-this.mouse.x));
+    //     // console.log( window.innerWidth)
+    //     // console.log("Mouth: " + this.mouse.x + " " + this.mouse.y);
+    //     // console.log("Mouth: " + event.clientX + " " + event.clientX);
+    //     // console.log("Sword: " + sword.position.x + " " + sword.position.y);
+    //     // console.log("Inter: " + this.pointOfIntersection.x + " " + this.pointOfIntersection.y)
+    // }
 }
 
 window.addEventListener('load', main);
@@ -281,14 +418,14 @@ window.addEventListener('load', main);
 /*  */
 function main() {  
     var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(125, window.innerWidth/window.innerHeight, 0.1, 1000);    
+    var camera = new THREE.PerspectiveCamera(125, window.innerWidth/window.innerHeight, 0.001, 2000);    
+    // var camera = new THREE.OrthographicCamera(window.innerWidth / 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / 2, 1, 1000)
     var renderer = new THREE.WebGLRenderer({antialias: true, canvas: document.querySelector("#bg")});
 
     var htmlControl = new HTMLControl();
     var swordCharacter = new SwordCharacter(scene, camera, renderer);
     var QGHAnimation = new Animation(scene, camera, renderer, htmlControl, swordCharacter);
 
-    QGHAnimation.coinFlip();
     QGHAnimation.sceneControl();
 
     document.getElementsByClassName("htmlContent-Container")[0].addEventListener("scroll", function() {
