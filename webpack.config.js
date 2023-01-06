e66c703b6f58560a81913a0c4d7c596243ef625c
QGh11 <Guanhua.Qiao@outlook.com>
2023-01-05 19:39:15 -0800

    webpack to dist

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const webpack = require("webpack");
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    entry: './client/js/app.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        },
+        {
+            test: /\.css$/,
+            use:
+            [
+                MiniCSSExtractPlugin.loader,
+                'css-loader'
+            ]
+        }]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        "window.jQuery": "jquery"
+        }),
+
+        new MiniCSSExtractPlugin()
+    ]
+}
